a5bb796a21a7b9ff0e3ef95edae733307ffc0264
Cemal Akif Özateş <70855621+salffev@users.noreply.github.com>
2023-03-05 15:39:33 +0300

    Add files via upload

---
+++ grup ödevi/ödeme akif.py
@@ -0,0 +1,23 @@
+import stripe
+
+# Stripe API anahtarını tanımlayın
+stripe.api_key = "sk_test_1234567890abcdefghijklmnopqrs"
+
+# Ödeme işlemi için müşteri ve kart bilgilerini belirleyin
+customer = stripe.Customer.create(email="test@example.com")
+card = stripe.Customer.create_source(customer.id, source="tok_visa")
+
+# Ödeme işlemi için gerekli parametreleri belirleyin
+charge = stripe.Charge.create(
+    amount=2000,
+    currency="para",
+    customer=customer.id,
+    source=card.id,
+    description="Test payment"
+)
+
+# Ödeme işlemi sonucunu kontrol edin
+if charge.status == "succeeded":
+    print("Payment succeeded!")
+else:
+    print("Payment failed.")
